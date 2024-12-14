46298177473810c112db2a7dd5b8f05624735691
Atlanticity91 <Atlanticity91@gmail.com>
2024-12-14 20:34:10 +0100

    Setup documentation branch

--- Build/Build-Dependencies.lua
+++
@@ -1,8 +0,0 @@
---- THIRDPARTY LIBRARIES
-IncludeDirs = { }
-
-IncludeDirs[ "glm" ] = "%{wks.location}/Thirdparty/glm/"
-IncludeDirs[ "libclang" ] = "%{wks.location}/Thirdparty/libclang/"
-IncludeDirs[ "spdlog" ] = "%{wks.location}/Thirdparty/spdlog/"
-IncludeDirs[ "yaml" ] = "%{wks.location}/Thirdparty/yaml-cpp/"
-IncludeDirs[ "test" ] = "%{wks.location}/MicroTest/"
